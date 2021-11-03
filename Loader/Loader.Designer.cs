d4d76b4ce173360b3b2f6c8e8cd07abd1d043c3c
dwiki <yohanesdwiki08@gmail.com>
2021-11-03 12:57:50 +0700

    Init

---
+++ Loader/Loader.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Loader
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.container = new System.Windows.Forms.Panel();
+			this.btnLoad = new System.Windows.Forms.Button();
+			this.nTray = new System.Windows.Forms.NotifyIcon(this.components);
+			this.container.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// container
+			// 
+			this.container.Controls.Add(this.btnLoad);
+			this.container.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.container.Location = new System.Drawing.Point(0, 0);
+			this.container.Name = "container";
+			this.container.Size = new System.Drawing.Size(944, 541);
+			this.container.TabIndex = 0;
+			// 
+			// btnLoad
+			// 
+			this.btnLoad.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnLoad.Location = new System.Drawing.Point(934, 528);
+			this.btnLoad.Name = "btnLoad";
+			this.btnLoad.Size = new System.Drawing.Size(10, 13);
+			this.btnLoad.TabIndex = 0;
+			this.btnLoad.UseVisualStyleBackColor = false;
+			this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+			// 
+			// nTray
+			// 
+			this.nTray.Icon = ((System.Drawing.Icon)(resources.GetObject("nTray.Icon")));
+			this.nTray.Text = "nTray";
+			this.nTray.Visible = true;
+			this.nTray.MouseClick += new System.Windows.Forms.MouseEventHandler(this.nTray_MouseClick);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(944, 541);
+			this.Controls.Add(this.container);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Main";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.container.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel container;
+		private System.Windows.Forms.NotifyIcon nTray;
+		private System.Windows.Forms.Button btnLoad;
+	}
+}
+
---
+++ Loader/Loader.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Loader
+{
+	public partial class Main : Form
+	{
+		private AxShockwaveFlashObjects.AxShockwaveFlash flashPlayer;
+		private OpenFileDialog openFileDialog;
+
+		public Main()
+		{
+			InitializeComponent();
+
+			flashPlayer = new AxShockwaveFlashObjects.AxShockwaveFlash();
+			flashPlayer.BeginInit();
+			flashPlayer.Name = "flashPlayer";
+			flashPlayer.Dock = DockStyle.Fill;
+			flashPlayer.TabIndex = 0;
+			container.Controls.Add(flashPlayer);
+			flashPlayer.EndInit();
+
+			//InitFlashEmbed();
+			InitFlashExternal();
+		}
+
+		private void InitFlashExternal()
+		{
+			string loader = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Loader.swf");
+			if (File.Exists(loader))
+				flashPlayer.LoadMovie(0, loader);
+		}
+
+		private void InitFlashEmbed()
+		{
+			byte[] swf = Properties.Resources.Loader;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+				{
+					binaryWriter.Write(8 + swf.Length);
+					binaryWriter.Write(1432769894);
+					binaryWriter.Write(swf.Length);
+					binaryWriter.Write(swf);
+					memoryStream.Seek(0L, SeekOrigin.Begin);
+					flashPlayer.OcxState = new AxHost.State(memoryStream, 1, false, null);
+				}
+			}
+		}
+
+		private void btnLoad_Click(object sender, EventArgs e)
+		{
+			openFileDialog = new OpenFileDialog();
+			openFileDialog.InitialDirectory = "C:\\";
+			openFileDialog.Filter = "SWF File (*.swf*)|*.swf*";
+			openFileDialog.Title = "Select .swf file to load";
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				flashPlayer.LoadMovie(0, " ");
+				flashPlayer.LoadMovie(0, openFileDialog.FileName);
+			}
+		}
+
+		private void nTray_MouseClick(object sender, MouseEventArgs e)
+		{
+			ShowForm(this);
+		}
+
+		private void ShowForm(Form form)
+		{
+			if (form.WindowState == FormWindowState.Minimized)
+			{
+				form.WindowState = FormWindowState.Normal;
+				form.Show();
+				form.BringToFront();
+				form.Focus();
+				return;
+			}
+			else if (form.Visible)
+			{
+				form.Hide();
+				return;
+			}
+			form.Show();
+			form.BringToFront();
+			form.Focus();
+		}
+	}
+}
